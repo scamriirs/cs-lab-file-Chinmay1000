9a3b2cc1b82ceb71ea07a3ac3dd47dea3d6652e7
Chinmay1000 <chinmay10nerurkar@gmail.com>
2025-05-13 19:15:27 +0530

    Create PrintNumberTo100usingDOwhile.cs

---
+++ PrintNumberTo100usingDOwhile.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int number = 1;
+        do
+        {
+            Console.WriteLine(number);
+            number++;
+        } while (number <= 100);
+    }
+}
